ef90b450106bf97c64591f1e41795ef2300cf551
psidrovo <paulsidrovob@gmail.com>
2022-12-03 09:42:20 -0500

    Inicio proyecto

---
+++ src/main/java/ec/edu/ups/InstaWallet/InstaWalletApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.ups.InstaWallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/ups/InstaWallet/InstaWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.ups.InstaWallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
