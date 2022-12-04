196eb925e85c55d2c673ac6c1cdfae25314d875b
adolfojara10 <gavilanesadolfo@gmail.com>
2022-12-04 18:06:24 -0500

    correccion paquete modelo

--- src/main/java/ec/edu/ups/InstaWallet/modelo/Credito.java
+++ src/main/java/ec/edu/ups/InstaWallet/modelo/Credito.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ec.edu.ups.modelo;
+package ec.edu.ups.InstaWallet.modelo;
 
 import java.util.ArrayList;
 import java.util.Date;
