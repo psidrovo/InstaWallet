d0a40a2761ce33f260234d11d2cb6d8ce8d023d9
adolfojara10 <gavilanesadolfo@gmail.com>
2022-12-07 19:12:33 -0500

    creacion clase para el swagger

---
+++ src/main/java/ec/edu/ups/InstaWallet/services/SwaggerApiConfig.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.ups.InstaWallet.services;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+
+/**
+ *
+ * @author EstAdolfoSebastianJa
+ */
+@Configuration
+public class SwaggerApiConfig {
+    
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("Practica 2.3").description(
+                        "This is a sample Spring Boot RESTful service using springdoc-openapi and OpenAPI 3."));
+    }
+}
