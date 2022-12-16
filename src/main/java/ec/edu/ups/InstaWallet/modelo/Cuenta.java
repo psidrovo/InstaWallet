574842fc0ddda406f5980996a2d9b1e3af0074f1
Romel Avila <ravilaf1@est.ups.edu.ec>
2022-12-15 20:51:53 -0500

    Creacion de Clases Cuenta y DetalleCuenta

---
+++ src/main/java/ec/edu/ups/InstaWallet/modelo/Cuenta.java
@@ -0,0 +1,82 @@
+package ec.edu.ups.InstaWallet.modelo;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
+public class Cuenta {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+	
+	private String numerCuenta;
+	private boolean estado;
+	private double monto;
+	private Date fechaCreacion;
+	
+	private List<Credito> creditos;
+	
+	@OneToMany(mappedBy = "cuentaID")
+	@JsonManagedReference
+	private List<DetalleCuenta> detallesCuentas;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNumerCuenta() {
+		return numerCuenta;
+	}
+
+	public void setNumerCuenta(String numerCuenta) {
+		this.numerCuenta = numerCuenta;
+	}
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+	public double getMonto() {
+		return monto;
+	}
+
+	public void setMonto(double monto) {
+		this.monto = monto;
+	}
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public List<Credito> getCreditos() {
+		return creditos;
+	}
+
+	public void setCreditos(List<Credito> creditos) {
+		this.creditos = creditos;
+	}
+	
+	
+
+}
---
+++ src/main/java/ec/edu/ups/InstaWallet/modelo/DetalleCuenta.java
@@ -0,0 +1,96 @@
+package ec.edu.ups.InstaWallet.modelo;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+public class DetalleCuenta {
+	
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	 private int id;
+	 
+	 private String tipoMovimiento;
+	 private Double valor;
+	 private Date fecha;
+	 
+	 @ManyToOne
+	 @JoinColumn(name = "cuentaID")
+	 @JsonBackReference 
+	 private Cuenta cuentaID;
+	 
+	 
+	 
+
+	public DetalleCuenta() {
+	}
+	
+	
+
+	public DetalleCuenta(int id, String tipoMovimiento, Double valor, Date fecha, Cuenta cuentaID) {
+		this.id = id;
+		this.tipoMovimiento = tipoMovimiento;
+		this.valor = valor;
+		this.fecha = fecha;
+		this.cuentaID = cuentaID;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTipoMovimiento() {
+		return tipoMovimiento;
+	}
+
+	public void setTipoMovimiento(String tipoMovimiento) {
+		this.tipoMovimiento = tipoMovimiento;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public Cuenta getCuentaID() {
+		return cuentaID;
+	}
+
+	public void setCuentaID(Cuenta cuentaID) {
+		this.cuentaID = cuentaID;
+	}
+
+	@Override
+	public String toString() {
+		return "DetalleCuenta [id=" + id + ", tipoMovimiento=" + tipoMovimiento + ", valor=" + valor + ", fecha="
+				+ fecha + ", cuentaID=" + cuentaID + "]";
+	}
+	 
+	 
+
+}
