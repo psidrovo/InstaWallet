bbc1da63fcdd70d932d9a8d28d5629007e11bd26
Denys Dutan <adriandutan15@gmail.com>
2022-12-29 14:00:29 -0500

    commit

--- src/test/java/ec/edu/ups/InstaWallet/test/CreditoServiceTest.java
+++ src/test/java/ec/edu/ups/InstaWallet/test/CreditoServiceTest.java
@@ -7,34 +7,23 @@ package ec.edu.ups.InstaWallet.test;
 import ec.edu.ups.InstaWallet.controller.CreditoRestController;
 import ec.edu.ups.InstaWallet.modelo.Credito;
 import ec.edu.ups.InstaWallet.modelo.DetalleCredito;
-import ec.edu.ups.InstaWallet.repository.CreditoRepo;
 import ec.edu.ups.InstaWallet.services.CreditoService;
-import io.swagger.v3.oas.annotations.servers.Server;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Optional;
 import java.util.stream.Stream;
-import javax.transaction.Transactional;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import static org.mockito.ArgumentMatchers.any;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -43,15 +32,6 @@ import org.springframework.web.context.request.ServletRequestAttributes;
  * @author EstAdolfoSebastianJa
  */
 public class CreditoServiceTest {
-/*
-    @Autowired
-    private CreditoRestController creditoRestController;
-    
-    @Autowired
-    private CreditoService creditoService;
-    
-    @Autowired
-    private CreditoRepo creditoRepo;*/
     
     @InjectMocks
     CreditoRestController creditoRestController;
@@ -65,20 +45,6 @@ public class CreditoServiceTest {
         MockitoAnnotations.openMocks(this);
         
     }
-
-   /* @ParameterizedTest
-    @MethodSource("generator")
-    public void shouldCreateContact(Credito cre) {
-        System.out.println(cre.toString());
-        
-        CreditoService creditoService = new CreditoService(creditoRepo);
-        CreditoRestController creditoRestController = new CreditoRestController(creditoService);
-        
-        Credito creditoGuardado = creditoRestController.crearCredito(cre);
-        System.out.println(creditoGuardado);
-        assertTrue(creditoGuardado.equals(cre));
-
-    }*/
     
     
     @ParameterizedTest
--- src/test/java/ec/edu/ups/InstaWallet/test/CuentaServiceTest.java
+++ src/test/java/ec/edu/ups/InstaWallet/test/CuentaServiceTest.java
@@ -1,19 +1,11 @@
 package ec.edu.ups.InstaWallet.test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -22,9 +14,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import ec.edu.ups.InstaWallet.controller.CuentaResController;
-import ec.edu.ups.InstaWallet.modelo.Credito;
 import ec.edu.ups.InstaWallet.modelo.Cuenta;
-import ec.edu.ups.InstaWallet.modelo.DetalleCredito;
 import ec.edu.ups.InstaWallet.services.CuentaService;
 
 
@@ -42,19 +32,6 @@ public class CuentaServiceTest {
         
     }
     
-    /*@ParameterizedTest
-    @MethodSource("generator")
-    public void crearCuenta(Cuenta cue){
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
-        
-        when(cuentaService.save(any(Cuenta.class))).thenReturn(cue);
-        
-        Cuenta response = cuentaResController.crearCuenta(cue);
-        
-        assertTrue(response.equals(cre));
-        
-    }*/
     
     @Test
     public void testListarCuentas(){
